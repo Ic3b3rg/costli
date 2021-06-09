1e4dcef1307bee80e6463c0a2e1badbe65a73368
Giuseppe Ettorre <g.ettorre@lswebmail.it>
2021-06-10 01:31:54 +0200

    fix: api url heroku

--- src/requestsQL/useTransaction.ts
+++ src/requestsQL/useTransaction.ts
@@ -3,7 +3,8 @@ import { useMutation, useQuery } from "react-query";
 //importa GraphQL-request client e GraphQueryLanguage(gql)
 import { GraphQLClient, gql } from "graphql-request";
 
-const API_URL = `http://localhost:4000/graphql`;
+// const API_URL = `http://localhost:4000/graphql`;
+const API_URL = `https://costli-be.herokuapp.com/graphql`;
 
 const graphQLClient = new GraphQLClient(API_URL);
 //     , {
