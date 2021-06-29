63d4368691021c839006a873aa0b2e2b87817f46
s.ceccarini <s.ceccarini>
2021-06-29 16:08:45 +0200

    fix

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-  
+const path = require('path');
 const express = require('express');
-
 const app = express();
-
-app.use(express.static('./build'));
-
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'build' }
-  );
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
+app.listen(port, () => {
+   console.log('Server is up!');
+});
