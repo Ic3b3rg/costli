c481d7d3797e4734f04986089c3429949e58cf3b
s.ceccarini <s.ceccarini>
2021-06-29 16:17:57 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join('build');
+console.log(publicPath)
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
